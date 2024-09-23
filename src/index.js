e796dbcc14974277424b706eefae816051cb7c79
gibbo85 <anthony.gibson85@gmail.com>
2024-09-23 08:40:19 +0100

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
+<Router basename="/emma-app">
+<Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/pattern" element={<Pattern />} />
+            <Route path="/:slug" element={
+              <ProtectedRoute element={<PatternDetail />} />
+            } />
+            <Route path="/:patternName/:step" element={
+              <ProtectedRoute element={<RoundDetail isCounterOpen={isCounterOpen} toggleCounter={toggleCounter}/>} />
+            } />
+            <Route path="/:patternName/complete" element={
+              <ProtectedRoute element={<PatternComplete />} />
+            } />
+            <Route path="/dashboard" element={
+              <ProtectedRoute element={<Dashboard />} />
+            } />
+</Router>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
