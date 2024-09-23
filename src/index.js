86c2ce73aeebc8d8af0fa669d75e9672d51b6d81
gibbo85 <anthony.gibson85@gmail.com>
2024-09-23 21:41:20 +0100

    more hash router bits

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouterRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename="/emma-app">
+    <Router >
     <App />
     </Router>
   </React.StrictMode>
